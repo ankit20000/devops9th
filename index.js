4eca6ab1a936731a80bb0923aa81a7a08a3c9420
ankit20000 <shuklaankit346@gmail.com>
2022-02-02 09:43:07 +0530

    added

--- index.js
+++ index.js
@@ -2,4 +2,11 @@ echo "hello index.js nkjbjskvfbsj"
 
 
 businees requirment -->
-manager --->> developer ----->>API -->>30days-->>done -->develop---git stash 
+manager --->> developer ----->
+
+echo "api"
+for i in [1 ,2 , 3 ,4]
+ do 
+   echo $i
+done 
+brjehbfjkdbh.er  
